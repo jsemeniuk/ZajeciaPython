4d6074f81a435d3239da336698682df0865e990e
Joanna Semeniuk <semeniuk.asia@gmail.com>
2018-12-01 20:37:12 +0100

    Dodanie obliczania ceny z VATem do zadania 1 z zajęć 3

--- zajecia3/zakupy.py
+++ zajecia3/zakupy.py
@@ -3,20 +3,22 @@ from zajecia1.VAT import vat_paragon
 
 def zakupy(cennik, lista):
     paragon = []
+    rachunek_bez_vat = 0
     for element in lista:
         if element in cennik:
             paragon.append(cennik[element] * lista[element])
-    # Zwraca tylko vat - poprawić
-    rachunek = vat_paragon(paragon)
+            rachunek_bez_vat += cennik[element] * lista[element]
+    vat = vat_paragon(paragon)
+    rachunek = rachunek_bez_vat + vat
     return rachunek
 
 
 cennik = {
-    'kawa' : 14.99,
-    'pomarańcze' : 3.49,
-    'olej' : 4.99
+    'kawa': 14.99,
+    'pomarańcze': 3.49,
+    'olej': 4.99
 }
 
-lista = {'olej' : 2, 'kawa' : 1}
+lista = {'olej': 2, 'kawa': 1}
 
 print(zakupy(cennik, lista))
