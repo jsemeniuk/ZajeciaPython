5cf11afeb84db108cdb0f102f3ed85490335f097
Joanna Semeniuk <semeniuk.asia@gmail.com>
2018-12-02 13:37:17 +0100

    Dodanie funkcji statystyka_slow do drugiego zadania domowego z zajęć 3

---
+++ zajecia3/statystyka_slow.py
@@ -0,0 +1,26 @@
+def statystyka_slow(ściezka):
+    # TODO odwoływanie się bezpośrednio do plików w internecie
+    stat = {}
+
+    with open(ściezka, 'r') as fh:
+        tekst = fh.read()
+
+    for akapit in tekst.split("\n"):
+        for slowo in akapit.split(" "):
+            slowo = slowo.lower()
+            if slowo[-1] in ".,!:;?":
+                slowo = slowo[:-1]
+
+            if slowo in stat:
+                stat[slowo] += 1
+            else:
+                stat[slowo] = 1
+    return stat
+
+# TODO Funkcja do prównywania tekstów
+def porownanie_tekstow():
+    return None
+
+print(statystyka_slow('plik_testowy'))
+
+
